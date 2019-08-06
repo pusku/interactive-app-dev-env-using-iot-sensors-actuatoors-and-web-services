40d396ba157122ed9a133d081a5ce977eeb5f739
pusku <puskuuuu@gmail.com>
2019-08-07 01:11:35 +0600

    Stable Version2

---
+++ static/js/generators/codegenerator-second_interactive_block.js
@@ -0,0 +1,8 @@
+
+            Blockly.Python['second_interactive_block'] = function(block) {var param0 = Blockly.Python.valueToCode(block, 'param0', Blockly.Python.ORDER_ATOMIC);
+var param1 = Blockly.Python.valueToCode(block, 'param1', Blockly.Python.ORDER_ATOMIC);
+
+                var code = 'second_interactive_block('+param0+','+param1+')';
+                return [code, Blockly.Python.ORDER_NONE];
+            };
+        
